c17a8241327c09016797df8d8429460ec4581980
Raphael Dietrich <12137107+xXKnightRiderXx@users.noreply.github.com>
2026-05-17 21:20:37 +0200

    Code formatting
    Code formatting with black

--- src/py_eforms_parser/objects/types.py
+++ src/py_eforms_parser/objects/types.py
@@ -1,10 +1,12 @@
 import enum
 
+
 class DocumentType(enum.StrEnum):
     """
-    Enum for the three different document types that the EFORMS standard defines:
+    Enum for the three different document types that the eForms standard defines:
     https://docs.ted.europa.eu/eforms/latest/schema/documents-forms-and-notices.html#documentAndDocumentTypeSection
     """
+
     CONTRACT = "ContractNotice"
     CONTRACT_AWARD = "ContractAwardNotice"
     PRIOR_INFORMATION = "PriorInformationNotice"
