675c7fbf46fd6dd8e466b6cd47ba92159d05114f
Raphael Dietrich <12137107+xXKnightRiderXx@users.noreply.github.com>
2026-05-17 13:58:44 +0200

    Add DocumentType enum
    The EFORMS standard differentiates between three different document types:  Contract, ContractAward and PriorInformation Notice. A corresponding enum has been added which will later be used to identify the document type.

---
+++ src/py_eforms_parser/objects/types.py
@@ -0,0 +1,10 @@
+import enum
+
+class DocumentType(enum.StrEnum):
+    """
+    Enum for the three different document types that the EFORMS standard defines:
+    https://docs.ted.europa.eu/eforms/latest/schema/documents-forms-and-notices.html#documentAndDocumentTypeSection
+    """
+    CONTRACT = "ContractNotice"
+    CONTRACT_AWARD = "ContractAwardNotice"
+    PRIOR_INFORMATION = "PriorInformationNotice"
