59890eb6bd8d2294a83d6e48a2c66b01b96624ec
Raphael Dietrich <12137107+xXKnightRiderXx@users.noreply.github.com>
2026-06-27 19:53:02 +0200

    EformsParser: introduce methods to return text values
    Add two methods to the EformsParser class that search an element with a given XPath expression and return its text value. These helper methods will be used in many future functions and avoid code duplication. Some fields are optional in the Eforms file and their absence is acceptable while other fields are required and their absence should abort the parsing process. To cover these two cases two function were created.

--- src/py_eforms_parser/parser/eformsparser.py
+++ src/py_eforms_parser/parser/eformsparser.py
@@ -24,6 +24,63 @@ class EformsParser:
         logger.debug(f"Trying to parse file: {eforms_file}")
         self.xml_tree = defusedxml.ElementTree.parse(eforms_file)
 
+    def _get_optional_text(
+        self, element: xml.etree.ElementTree.Element, xpath_expression: str, default: str | None = None
+    ) -> str | None:
+        """Search an element with an XPath expression and return its text value or default if the node does not exist
+
+        This function takes an element that is part of the overall tree and searches for a specific subnode according to
+        the given XPath expression. If such a node can be found and has a text value the function returns it. If the
+        searched node does no exist or if the node is empty the function will return the given default value or None if
+        no value is passed. Intended for fields that are optional in the eForms file as a missing element is treated as
+        a normal condition and will not raise an exception.
+
+        Args:
+            element (xml.etree.ElementTree.Element): Element of the tree in which the find operation should be performed
+            xpath_expression (str): The XPath expression of the sub-object that should be returned
+            default (str | None, optional): _description_. Defaults to None.
+
+        Returns:
+            str | None: The text value of the requested object or the default value if it's not found
+        """
+        logger.debug(f"Searching in element {element.tag} with path {xpath_expression}")
+        node = element.find(xpath_expression, NAMESPACES)
+        if node is None:
+            logger.debug(f"Node could not be found. Returning default value: {default}")
+            return default
+        # The node can be present but empty. in such cases node.text is None
+        # We return the default value for now but theoretically an empty string would also be possible
+        elif node.text is None:
+            logger.debug(f"Node found but it has no content. Returning default value {default}")
+            return default
+        else:
+            logger.debug(f"Node found. Returning its content: {node.text}")
+            return node.text
+
+    def _get_required_text(self, element: xml.etree.ElementTree.Element, xpath_expression: str) -> str:
+        """Search an element with an XPath expression and return its text value
+
+        This function takes an element that is part of the overall tree and searches for a specific subnode according to
+        the given XPath expression. If such a node can be found and has a text value the function returns it. If the
+        searched node does no exist a ValueError is raised. Intended for fields that are required in the eForms file and
+        a missing element should lead to abortion of the parsing process.
+
+        Args:
+            element (xml.etree.ElementTree.Element): Element of the tree in which the find operation should be performed
+            xpath_expression (str): The XPath expression of the sub-object that should be returned
+
+        Raises:
+            ValueError: If the given XPath expression can not be found in the given element
+
+        Returns:
+            str: The text value of the requested object
+        """
+        node_text = self._get_optional_text(element, xpath_expression)
+        if node_text is None:
+            logger.error(f"Required path {xpath_expression} could not be found in {element.tag}. Raising ValueError")
+            raise ValueError(f"Required element {xpath_expression} not found in {element.tag}")
+        return node_text
+
     def _parse_document_type(self, root_element: xml.etree.ElementTree.Element) -> DocumentType:
         """Parse the document type of the eForms file
 
